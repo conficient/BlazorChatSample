8a3bebcc4030b29b1d066fa44a31abb3370ac05c
Howard Richards <dev@conficient.com>
2019-07-12 10:28:30 +0100

    Amend startup to fix #14

--- BlazorChatSample.Server/Startup.cs
+++ BlazorChatSample.Server/Startup.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
+using System.IO;
 using System.Linq;
 
 namespace BlazorChatSample.Server
@@ -41,16 +43,47 @@ namespace BlazorChatSample.Server
 
             app.UseRouting();
 
+            const string projectName = "blazorchatsample";
+            ConfigureStatics(app, projectName);
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
                 // new SignalR endpoint routing setup
                 endpoints.MapHub<Hubs.ChatHub>("/chathub");
                 // new preview 6 startup for Blazor
-                endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
+                MapBlazorFallback(endpoints, projectName);
             });
 
         }
 
+        private static void ConfigureStatics(IApplicationBuilder app, string projectName)
+        {
+#if DEBUG
+            app.UseStaticFiles();
+
+#else
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot/_content/{projectName}")),
+                RequestPath = new Microsoft.AspNetCore.Http.PathString("")
+            });
+#endif
+        }
+
+        private static void MapBlazorFallback(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder endpoints, string projectName)
+        {
+            //This is a workaround for an issue with p6 that creates a different folder
+            //structure on publish than the one expected by the blazor fallback
+            //for more info go to
+            // https://github.com/aspnet/AspNetCore/issues/11185
+            string blazorFallbackPath;
+#if DEBUG
+            blazorFallbackPath = "index.html";
+#else
+            blazorFallbackPath = $"_content/{projectName}/index.html";
+#endif
+            endpoints.MapFallbackToClientSideBlazor<Client.Startup>(blazorFallbackPath);
+        }
     }
 }
