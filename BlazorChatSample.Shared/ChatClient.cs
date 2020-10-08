57a6ff7b05933037241a7f428a72706b9d4d627d
Michael AHS <michael.ahs@soprasteria.com>
2020-10-08 22:59:48 +0200

    Migrated to .Net 5.0-rc1

--- BlazorChatSample.Shared/ChatClient.cs
+++ BlazorChatSample.Shared/ChatClient.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.SignalR.Client;
+﻿using Microsoft.AspNetCore.SignalR.Client;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BlazorChatSample.Shared
